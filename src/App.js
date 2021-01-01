560801b5e3b01e975e51f1816110664f6761c9a9
callthecops <tudor_rimbetiu@yahoo.com>
2021-01-02 00:48:13 +0200

    added first compoents

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Route, BrowserRouter as Router, Link, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import CarList from './components/CarList';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <Link to="/carList">Car List</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/home" component={Home}>Home</Route>
+          <Route path="/carList" component={CarList}>Car List</Route>
+        </Switch>
+
+        <header className="App-header">
+
+        </header>
+      </div>
+    </Router>
   );
 }
 
