ba7b523d081ccab125741217d311f219856f1be5
callthecops <tudor_rimbetiu@yahoo.com>
2021-01-02 14:31:31 +0200

    add

---
+++ src/components/CarList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class CarList extends Component {
+    render() {
+        return (
+            <div>
+                CarList
+            </div>
+        )
+    }
+}
+
+export default CarList
